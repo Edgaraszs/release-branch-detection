0988e1df0e4103c6314e6329da1ba78487065caf
Edgaraszs <edgas258@gmail.com>
2024-03-01 12:57:22 +0200

    feat: rename release to archive

--- release-branch-detection.sh
+++ release-branch-detection.sh
@@ -14,15 +14,48 @@ get_release_branches() {
     echo "${branches}" | jq -r '.[] | select(.name | startswith("release")) | .name'
 }
 
+rename_to_archive() {
+    local branch="$1"
+
+    echo "Renaming branch: ${branch} to archive/${branch}"
+        curl -s \
+            -X POST \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer ${REPO_TOKEN}" \
+            "https://api.github.com/repos/${REPO}/branches/${branch}/rename" \
+            -d '{"new_name":"archive/'"${branch}"'"}' >/dev/null
+}
+
 main() {
     local release_branches
     release_branches=$(get_release_branches)
     echo "Release branches: ${release_branches}"
 
+    local -A release_versions=()
+
     while IFS= read -r branch; do
-        version=$(echo "$branch" | sed 's/release\/v//')
-        echo "Version number for branch $branch: $version"
+        version=$(echo "$branch" | sed 's/release\/v//; s/[^0-9]//g')
+
+        release_versions["$branch"]="$version"
     done <<< "$release_branches"
+
+    highest_version=0
+
+    for branch in "${!release_versions[@]}"; do
+        version="${release_versions[$branch]}"
+        if (( version > highest_version )); then
+            highest_version="$version"
+        fi
+    done
+
+    for branch in "${!release_versions[@]}"; do
+        version="${release_versions[$branch]}"
+        if (( version != highest_version )); then
+            rename_to_archive $branch
+        fi
+    done
+
+    echo "Highest release version: $highest_version"
 }
 
 main '$@'
